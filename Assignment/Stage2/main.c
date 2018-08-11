7636bc6112f3565cfb9ec6044ff976182c3b3d85
hacker-guy <bugeeja@gmail.com>
2018-08-11 11:21:48 +0200

    Add to GitHub

---
+++ Assignment/Stage2/main.c
@@ -0,0 +1,109 @@
+/* COMP20003
+ * Yelp Database
+ * Author: <Justin Bugeja> <jbugeja> <758397>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include "dictionary2.h"
+
+//Main program
+int main(int argc, char *argv[]) {
+	char *filename, *filekeysname;
+	FILE *file, *filekeys;
+	char buffer1[NAME_SIZE+1] = {0}, buffer2[DATA_SIZE+4] = {0}, c;
+	dict_t *dict;
+	int i=0, j=0, buffer=1;
+	
+	//Return error if no dictionary or key search files
+	if (argc != 3){
+		printf("ERROR\n");
+		exit(EXIT_FAILURE);
+	}
+	filename = argv[1];
+	//Return error if dictionary file cannot be opened
+	if ( (file = fopen(filename, "r")) == NULL ) {
+		printf("ERROR\n\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	//Create the dictionary and return error if not created
+	printf("Creating dictionary...\n");
+	if ((dict = createDict()) == NULL) {
+		printf("ERROR in creating dictionary\n\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("Empty dictionary created\n");
+	
+	
+	//Read in from a file
+	while ((c = fgetc(file)) != EOF) {
+		if (c == DELIMINATOR) {
+			//Switch buffer
+			buffer = 2;
+			i = 0;
+			//Clear buffer
+			memset(buffer2, 0, sizeof(buffer2));
+			continue;
+			/* Continue so character isn't read into buffer*/
+		}
+		if (buffer == 1) {
+			buffer1[i] = c;
+			i++;
+		} else if (buffer == 2) {
+			buffer2[j] = c;
+			j++;
+		}
+		if (c == NEW_LINE) {
+			//Switch buffer
+			buffer = 1;
+			//Insert record into dictionary
+			insert(dict, createRec(buffer1, buffer2));
+			j = 0;
+			//Clear buffer
+			memset(buffer1, 0, sizeof(buffer1));
+		}	
+    }
+	fclose(file);
+	
+	printf("Successfully filled dictionary\n");
+	
+	filekeysname = argv[2];
+	//Open the keys file
+	if ( (filekeys = fopen(filekeysname, "r")) == NULL ) {
+		printf("ERROR\n\n");
+		exit(EXIT_FAILURE);
+	}
+	//If output file if already created then remove it
+	if( access( OUTPUTFILE, F_OK ) != -1 ) {
+    	if (remove( "output.txt" )==-1) {
+    		printf("ERROR\n\n");
+    		exit(EXIT_FAILURE);
+    	} else {
+    		printf("~Previous output file deleted\n");
+    	}
+    }
+    //Read keys file
+	while ((c = fgetc(filekeys)) != EOF) {
+		if (c == NEW_LINE) {
+			//Search for the key in the dictionary
+			search(buffer1, dict);
+			i = 0;
+			//Clear the buffer to read next key
+			memset(buffer1, 0, sizeof(buffer1));
+			continue;
+		}
+		buffer1[i++] = c;
+	}
+	fclose(filekeys);
+	
+	//Free tree from memory
+	free_tree(dict->head);
+	free(dict);
+	
+	return 0;
+}
---
+++ Stage2/dictionary2.c
@@ -0,0 +1,176 @@
+/* COMP20003
+ * Yelp Database
+ * Author: <Justin Bugeja> <jbugeja> <758397>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+#include "dictionary2.h"
+
+//Creates the dictionary struct node and returns its address
+dict_t *createDict() {
+	dict_t *pd = malloc(sizeof(dict_t));
+	assert(pd);
+	pd->head = NULL;
+	return pd;
+}
+
+//Create a seperate record for the data and copy info over
+record_t *createRec(char *name, char *data) {
+	
+	record_t *pr = malloc(sizeof(record_t));
+	pr->left = NULL;
+	pr->right = NULL;
+	pr->nextkey = NULL;
+	pr->name = strdup(name);
+	pr->data = strdup(data);
+	
+	assert(pr);
+	
+	return pr;
+}
+
+/* Change the address of the first record to point at the 
+second record */
+void changeAddress(record_t **p1, record_t *p2) {
+ *p1 = p2;
+}
+
+//Reads in a record and places it in the dictionary
+void insert( dict_t *pd, record_t *x) {
+	//Checks if first element
+	int compare;
+	if(pd->head == NULL) {
+		changeAddress(&pd->head, x);
+		
+	} else {
+		
+		record_t *pr = pd->head;
+		
+		//While not at the end of the dictionary
+		while (1) {
+			compare = strcmp(x->name, pr->name);
+			//If right
+			if (compare > 0) {
+				if(pr->right == NULL) {
+					changeAddress(&pr->right, x);
+					return;
+				}
+				//Point to next node
+				pr = pr->right;
+			//If left
+			} else if (compare < 0){
+				if(pr->left == NULL) {
+					changeAddress(&pr->left, x);
+					return;
+				}
+				//Point to next node
+				pr = pr->left;
+			//If equal
+			} else if (compare == 0){
+				while (pr->nextkey != NULL) {
+					//Point to next node
+					pr = pr->nextkey;
+				}
+				changeAddress(&pr->nextkey, x);
+				return;
+			}
+		}
+	}
+}
+
+//Searches the dictionary for a given key
+void search(char *name, dict_t *pd) {
+	int compare, cmp = 0, found = 0;
+	
+	//Check if the tree has a head
+	if(pd->head == NULL) {
+		printf("no tree found");
+		exit(EXIT_FAILURE);
+	} else {
+		
+		//Point to the head of the tree
+		record_t *pr = pd->head;
+
+		//While not at the end of the tree
+		while (1) {
+			compare = strcmp(name, pr->name);
+			cmp++;
+			//If right
+			if (compare > 0) {
+				if(pr->right == NULL) {
+					break;
+				}
+				//Point to next node
+				pr = pr->right;
+				//If left
+			} else if(compare < 0) {
+				if (pr->left == NULL) {
+					break;
+				}
+				//Point to next node
+				pr = pr->left;
+				//If found
+			} else if(compare == 0) {
+				printToFile(pr, cmp); 
+				found++;
+				//While there is still keys in the list
+				while (pr->nextkey != NULL) {
+					pr = pr->nextkey;
+					printToFile(pr, cmp);
+				}
+				break;
+			} else {
+				printf("error in strcmp\n");
+			}
+		}
+	} 
+	//If the record wasn't found print not found
+	if(!found) {
+		printNotFound(name);
+	}
+	printf("%s --> %d\n", name, cmp);
+}
+
+//Print not found to file
+void printNotFound(char *name) {
+	FILE *fp;
+	fp=fopen(OUTPUTFILE,"ab");
+	fprintf(fp, "%s --> NOTFOUND\n\n", name);
+	fclose(fp);
+}
+
+//Print the key and data to file
+void printToFile(record_t *pr, int cmp) {
+	FILE *fp;
+	fp=fopen(OUTPUTFILE,"ab");
+	fprintf(fp, "%s --> %s\n", pr->name, pr->data);
+	fclose(fp);
+}
+	
+//Output the key and number of comparisons
+void printKey(record_t *node, int cmp) {
+	printf("%s --> %d\n", node->name, cmp);
+}
+
+//Free the allocated memory
+void free_tree(record_t *node) {
+	if (node == NULL) {
+		return;
+	}
+	if (node->nextkey != NULL) {
+		//Traverse through the list before freeing
+		free_tree(node->nextkey);
+	}
+	//Post-order traversal
+	free_tree(node->left);
+	free_tree(node->right);
+	
+	//Free node elements then free node
+	free(node->name);
+	free(node->data);
+	free(node);
+}
